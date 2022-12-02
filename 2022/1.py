d7f6d511f516d0aed8ec0e7e5aab5afe4c6e01c0
Madeleine Goebel <madeleine.goebel@gmail.com>
2022-12-02 15:43:47 -0500

    Solved puzzle two and refactored into a function

--- 2022/1.py
+++ 2022/1.py
@@ -1,26 +1,35 @@
-# argv.py
 import sys
 
-if (len(sys.argv) < 1):
-    print("I need an input file. Please try again :)")
+if (len(sys.argv) < 2):
+    print("I need an input file and a number of elves. Please try again :)")
     exit()
+
+def sumElvesWithMostCalories(input_file, num_elves):
+    input_file = open(sys.argv[1], "r")
+    elves = input_file.read().split("\n\n")
+
+    calorie_counts = []
+
+    for elf in elves:
+        calorie_count = sum([int(snack) for snack in elf.split("\n") if type(snack)== int or snack.isdigit()])
+        calorie_counts += [calorie_count]
+
+    calorie_counts.sort(reverse=True)
+
+    total = 0
+    for i in range(num_elves):
+        total += calorie_counts[i]
+
+    print(total)
+
 
 print("Puzzle One:")
 
-
-input_file = open(sys.argv[1], "r")
-elves = input_file.read().split("\n\n")
-
-max_snack = -1
-
-for elf in elves:
-    calorie_count = sum([int(snack) for snack in elf.split("\n") if type(snack)== int or snack.isdigit()])
-    if calorie_count > max_snack:
-        max_snack = calorie_count
-
-print(max_snack)
+sumElvesWithMostCalories(sys.argv[1], 1)
 
 print("Puzzle Two:")
+
+sumElvesWithMostCalories(sys.argv[1], int(sys.argv[2]))
 
 
 
